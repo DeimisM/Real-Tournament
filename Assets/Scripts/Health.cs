b9524cea40396963eecea4da52f9941656326734
DeimisM <deimis.mardosas@outlook.com>
2024-02-18 16:48:50 +0200

    health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int hp;
+    public int maxHp = 100;
+
+    private void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,8 +6,18 @@ public class Rocket : MonoBehaviour
 {
     public float speed = 20;
 
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
     private void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
     }
 }
