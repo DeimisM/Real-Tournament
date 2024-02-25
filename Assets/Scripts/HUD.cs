394cf76eb0e45662d6ffc9f19d5f2d9c1383d50b
DeimisM <deimis.mardosas@outlook.com>
2024-02-25 17:58:50 +0200

    UI, other upgrades etc

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField]TMP_Text ammoText;
+    [SerializeField]TMP_Text healthText;
+
+    public Weapon weapon;
+    public Health health;
+
+    private void Start()
+    {
+        UpdateUI();
+        weapon.onShoot.AddListener(UpdateUI);
+        health.onDamage.AddListener(UpdateUI);
+    }
+
+    private void UpdateUI()
+    {
+        ammoText.text = weapon.clipAmmo + "/" + weapon.ammo;
+        healthText.text = health.hp.ToString();
+    }
+}
